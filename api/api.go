4297fa089bc12574e88d132ad4b8a31637feb688
nukeolay <66204571+nukeolay@users.noreply.github.com>
2021-11-06 13:06:37 +0300

    working on insta nil

--- api/api.go
+++ api/api.go
@@ -1,11 +1,16 @@
 package api
 
 import (
-	"errors"
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/ahmdrz/goinsta/v2"
 )
+
+var LoginRequiredError = goinsta.ErrorN{"login_required", "fail", ""}
+var UserNotFoundError = goinsta.ErrorN{"User not found", "fail", "user_not_found"}
 
 func GetPrivateStatus(insta *goinsta.Instagram, username string) (isPrivate bool, err error) {
 	igUser, err := insta.Profiles.ByName(username)
@@ -16,23 +21,132 @@ func GetPrivateStatus(insta *goinsta.Instagram, username string) (isPrivate bool
 	}
 }
 
-func GetApi(username string, password string) (insta *goinsta.Instagram, err error) {
-	insta, errLoad := goinsta.Import(".goinsta")
-	if errLoad != nil {
-		log.Printf("INSTA import error: %v", errLoad)
-		insta = goinsta.New(username, password)
-		errLogin := insta.Login()
-		if errLogin != nil {
-			log.Printf("INSTA login error: %v", errLogin)
-			return nil, errors.New("login error")
-		} else {
-			log.Printf("INSTA login success")
-			errExport := insta.Export(".goinsta")
-			if errExport != nil {
-				log.Printf("INSTA export error: %v", errLoad)
-			}
-			return insta, nil
-		}
+func LoadLogins() (igAccounts map[string]string) {
+	file, err := ioutil.ReadFile(".igAccounts")
+	if err != nil {
+		log.Printf("IGLOGINS ERROR load %v", err)
+		os.Exit(3)
+		return nil
+	} else {
+		json.Unmarshal([]byte(file), &igAccounts)
+		return igAccounts
 	}
-	return insta, nil
 }
+
+
+
+func GetSavedApi(igAccounts map[string]string) *goinsta.Instagram {
+	insta, errLoad := goinsta.Import(".goinsta")
+	if errLoad != nil { // если ошибка импорта
+		log.Printf("INSTA ERROR import: %v", errLoad)
+		return insta // возвращаю или ноль, или залогиненный
+	}
+	log.Print("INSTA import success")
+	return insta // возвращаю залогиненный по импорту
+}
+
+func GetNewApi(igAccounts map[string]string) *goinsta.Instagram {
+	insta := InstaLogin(igAccounts)
+	if insta != nil {
+		InstaExport(insta)
+	}
+	return insta
+}
+
+func InstaExport(insta *goinsta.Instagram) {
+	errExport := insta.Export(".goinsta")
+	if errExport != nil {
+		log.Printf("INSTA export error: %v", errExport)
+	} else {
+		log.Printf("INSTA export success")
+	}
+}
+
+func InstaLogin(igAccounts map[string]string) *goinsta.Instagram {
+	var insta *goinsta.Instagram
+	var errLogin error
+	for igLogin, igPassword := range igAccounts {
+		insta = goinsta.New(igLogin, igPassword)
+		errLogin = insta.Login()
+		if errLogin != nil {
+			log.Printf("LOGIN ERROR with %s: %v ", igLogin, errLogin)
+			continue
+		}
+		log.Printf("LOGIN success with %s", igLogin)
+		return insta
+	}
+	log.Println("LOGIN ERROR all attempts fail")
+	return nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO скорее всего эту функцию нужно использовать только, когда возникает ошибка с входом
+//TODO а при загрузке программы нужно использовать что-то попроще, где не происходит обязательного логина
+//TODO типа такого
+
+// func GetApi(igAccounts map[string]string) *goinsta.Instagram {
+// 	insta, errLoad := goinsta.Import(".goinsta")
+// 	if errLoad != nil { // если ошибка импорта
+// 		log.Printf("INSTA ERROR import: %v", errLoad)
+// 		insta = InstaLogin(igAccounts)
+// 		if insta != nil {
+// 			InstaExport(insta)
+// 		}
+// 		return insta // возвращаю или ноль, или залогиненный
+// 	}
+// 	//TODO возможно этот кусоу кода не нужно включать?
+// 	//TODO если импорт удался, то можно отдавать залогиненный ранее файл
+// 	//TODO а проверка на логин будет проводиться
+
+// 	errLogin := insta.Login()
+
+// 	if errLogin != nil { // если ошибка входа с импортированным файлом
+// 		insta = InstaLogin(igAccounts)
+// 		if insta != nil {
+// 			InstaExport(insta)
+// 		}
+// 		return insta // возвращаю или ноль, или залогиненный
+// 	}
+
+// 	log.Print("LOGIN success with import")
+// 	return insta // возвращаю залогиненный по импорту
+// }
+
+
+
+// func GetApi(username string, password string) (insta *goinsta.Instagram, err error) {
+// 	insta, errLoad := goinsta.Import(".goinsta")
+// 	if errLoad != nil {
+// 		log.Printf("INSTA import error: %v", errLoad)
+// 		insta = goinsta.New(username, password)
+// 		errLogin := insta.Login()
+// 		if errLogin != nil {
+// 			log.Printf("INSTA login error: %v", errLogin)
+// 			return nil, errors.New("login error")
+// 		} else {
+// 			log.Printf("INSTA login success")
+// 			errExport := insta.Export(".goinsta")
+// 			if errExport != nil {
+// 				log.Printf("INSTA export error: %v", errLoad)
+// 			}
+// 			return insta, nil
+// 		}
+// 	}
+// 	return insta, nil
+// }
--- main.go
+++ main.go
@@ -27,10 +27,6 @@ type Config struct {
 	UpdateStatusPeriod int
 	UpdateNextAccount  int
 	Port               string
-	IgUsername1        string
-	IgPassword1        string
-	IgUsername2        string
-	IgPassword2        string
 }
 
 func getConfig() Config {
@@ -66,29 +62,29 @@ func loadData(config Config) {
 	}
 }
 
-func task(bot *tgbotapi.BotAPI, insta *goinsta.Instagram, db map[int64]account, config Config) {
+func taskTryLogin() {
+
+}
+
+func taskUpdateStatus(bot *tgbotapi.BotAPI, insta *goinsta.Instagram, db map[int64]account, igAccounts map[string]string, config Config) {
+	time.Sleep(time.Duration(config.UpdateNextAccount * 1000000000)) // пауза перед запуском таска
 	log.Printf("CRON started")
 	for chatId, storedAccounts := range db {
 		for accountName, oldPrivateStatus := range storedAccounts {
 			log.Printf("CRON updating %s", accountName)
 			newPrivateStatus, err := api.GetPrivateStatus(insta, strings.ToLower(accountName))
-			if err != nil { // любая ошибка при проверке статуса
+			if err == api.UserNotFoundError { // ошибка "account_not_found"
+				continue
+			} else if err != nil { // ошибка при проверке статуса кроме "account_not_found"
 				log.Printf("CRON ERROR updating %s, %v", accountName, err)
-				insta, err = api.GetApi(config.IgUsername1, config.IgPassword1)
-				if err != nil { // ошибка авторизации
-					log.Printf("CRON ERROR login %s, %v", config.IgUsername1, err)
-					insta, err = api.GetApi(config.IgUsername2, config.IgPassword2)
-					if err != nil { // ошибка авторизации
-						log.Printf("CRON ERROR login %s, %v", config.IgUsername2, err)
-						break
-					} else {
-						newPrivateStatus, err = api.GetPrivateStatus(insta, strings.ToLower(accountName))
-						if err != nil { // это ошибка не связанная с логином, возможно поменялось имя акканта, пропустить обновление
-							log.Printf("CRON ERROR updating %s, %v", accountName, err)
-							continue
-						}
-					}
-				} else {
+				insta = api.GetNewApi(igAccounts)
+				if insta == nil { // ошибка авторизации
+					//TODO надо выработать единые правила для нулевой инсты - брать паузу для логина (как-то через крон) или выключать бота
+					//TODO если инста нулевая, то потом может произойти что угодно,
+					//TODO тут надо прекращать таск и брать паузу для логина (как-то через крон) или выключать бота
+					log.Print("CRON ERROR login")
+					continue
+				} else { // авторизация прошла успешно
 					newPrivateStatus, err = api.GetPrivateStatus(insta, strings.ToLower(accountName))
 					if err != nil { // это ошибка не связанная с логином, возможно поменялось имя акканта, пропустить обновление
 						log.Printf("CRON ERROR updating %s, %v", accountName, err)
@@ -119,6 +115,8 @@ func MainHandler(resp http.ResponseWriter, _ *http.Request) {
 }
 
 func main() {
+	isPanic := false
+	isTryingToLogin := false
 	config := getConfig()
 	port := config.Port
 	if config.Port == "" {
@@ -136,12 +134,14 @@ func main() {
 	log.SetOutput(f)
 
 	// instagram login
-	insta, err := api.GetApi(config.IgUsername1, config.IgPassword1)
-	if err != nil {
-		log.Printf("INSTA error getApi: %v", err)
-		insta, err = api.GetApi(config.IgUsername2, config.IgPassword2)
-		if err != nil {
-			log.Panic(err)
+	igAccounts := api.LoadLogins()
+	insta := api.GetSavedApi(igAccounts)
+	if insta == nil { // не получилось импортировать
+		insta = api.GetNewApi(igAccounts)
+		if insta == nil { // не получилось залигиниться
+			//TODO надо выработать единые правила для нулевой инсты - брать паузу для логина (как-то через крон) или выключать бота
+			////isPanic = true
+			log.Panicf("ERROR ERROR ERROR INSTA IS NIL")
 		}
 	}
 
@@ -164,143 +164,168 @@ func main() {
 
 	//setting up cron update accounts
 	s := gocron.NewScheduler(time.UTC)
-	_, errS := s.Every(config.UpdateStatusPeriod).Minutes().Do(task, bot, insta, db, config)
+	_, errS := s.Every(config.UpdateStatusPeriod).Minutes().Do(taskUpdateStatus, bot, insta, db, igAccounts, config)
 	if errS != nil {
 		log.Printf("CRON ERROR update status %v", errS)
 	}
 	s.StartAsync()
 
+	if isPanic {
+
+	}
+
 	for update := range updates {
-
-		if update.CallbackQuery != nil { // обработка нажатий на кнопки в телеграме
-			bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data))
-			var chatId int64 = update.CallbackQuery.Message.Chat.ID
-			delete(db[chatId], update.CallbackQuery.Data)
-			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_deleted"], update.CallbackQuery.Data))
-			msg.ParseMode = "HTML"
-			saveData(db, config)
-			bot.Send(msg)
-			if len(db[chatId]) == 0 {
-				msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, assets.Texts["account_list_is_empty_now"])
-				bot.Send(msg)
+		if isPanic {
+			if update.Message == nil { // игнорируем все кроме сообщений
+				continue
 			}
-			continue
-		}
-
-		if update.Message == nil { // игнорируем все кроме сообщений
-			continue
-		}
-		var chatId int64 = update.Message.Chat.ID
-		if update.Message.Command() != "" {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			switch update.Message.Command() {
-			case "start":
-				log.Printf("COMMAND /start %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
-				msg.ParseMode = "HTML"
-				msg.Text = assets.Texts["instructions"]
-			case "help":
-				log.Printf("COMMAND /help %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
-				msg.ParseMode = "HTML"
-				msg.Text = assets.Texts["instructions"]
-			case "SuperGetUserNumber":
-				log.Printf("COMMAND /secret %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
-				msg.Text = fmt.Sprintf("%d", len(db))
-			case "accounts":
-				log.Printf("COMMAND /accounts %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
-				accountsOutput := ""
-				for eachAccount, isPrivate := range db[chatId] {
-					statusEmoji := "🟢 "
-					if isPrivate {
-						statusEmoji = "🔴 "
-					}
-					accountsOutput = accountsOutput + statusEmoji + " " + eachAccount + "\n\n"
-				}
-				if accountsOutput == "" {
-					msg.Text = assets.Texts["account_list_is_empty"]
-				} else {
-					msg.Text = accountsOutput
-				}
-			case "del":
-				log.Printf("COMMAND /del %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
-				deleteAccountsKeyboard := tgbotapi.InlineKeyboardMarkup{}
-				for eachAccount := range db[chatId] {
-					var row []tgbotapi.InlineKeyboardButton
-					button := tgbotapi.NewInlineKeyboardButtonData(eachAccount, eachAccount)
-					row = append(row, button)
-					deleteAccountsKeyboard.InlineKeyboard = append(deleteAccountsKeyboard.InlineKeyboard, row)
-				}
-
-				msg.Text = assets.Texts["account_choose_to_delete"]
-				msg.ReplyMarkup = deleteAccountsKeyboard
-			default:
-				log.Printf("COMMAND UNKNOWN %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
-				msg.Text = assets.Texts["unknown_command"]
-			}
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, assets.Texts["panic"])
 			bot.Send(msg)
 		} else {
-			// тут добавляем новый аккаунт
-			log.Printf("ADD try %s BY %s (ID %d)", update.Message.Text, update.Message.From.UserName, update.Message.From.ID)
-			if _, ok := db[chatId]; !ok {
-				db[chatId] = make(account)
-			}
-
-			if len(db[chatId]) > 2 {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, assets.Texts["limit_of_accounts"])
+			if update.CallbackQuery != nil { // обработка нажатий на кнопки в телеграме
+				bot.AnswerCallbackQuery(tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data))
+				var chatId int64 = update.CallbackQuery.Message.Chat.ID
+				delete(db[chatId], update.CallbackQuery.Data)
+				msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_deleted"], update.CallbackQuery.Data))
+				msg.ParseMode = "HTML"
+				saveData(db, config)
 				bot.Send(msg)
+				if len(db[chatId]) == 0 {
+					msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, assets.Texts["account_list_is_empty_now"])
+					bot.Send(msg)
+				}
 				continue
 			}
 
-			if strings.Contains(update.Message.Text, " ") {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], assets.Texts["account_not_found"]))
-				bot.Send(msg)
+			if update.Message == nil { // игнорируем все кроме сообщений
 				continue
 			}
+			var chatId int64 = update.Message.Chat.ID
+			if update.Message.Command() != "" {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+				switch update.Message.Command() {
+				case "start":
+					log.Printf("COMMAND /start %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
+					msg.ParseMode = "HTML"
+					msg.Text = assets.Texts["instructions"]
+				case "help":
+					log.Printf("COMMAND /help %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
+					msg.ParseMode = "HTML"
+					msg.Text = assets.Texts["instructions"]
+				case "SuperGetUserNumber":
+					log.Printf("COMMAND /secret %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
+					msg.Text = fmt.Sprintf("%d", len(db))
+				case "accounts":
+					log.Printf("COMMAND /accounts %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
+					accountsOutput := ""
+					for eachAccount, isPrivate := range db[chatId] {
+						statusEmoji := "🟢 "
+						if isPrivate {
+							statusEmoji = "🔴 "
+						}
+						accountsOutput = accountsOutput + statusEmoji + " " + eachAccount + "\n\n"
+					}
+					if accountsOutput == "" {
+						msg.Text = assets.Texts["account_list_is_empty"]
+					} else {
+						msg.Text = accountsOutput
+					}
+				case "del":
+					log.Printf("COMMAND /del %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
+					deleteAccountsKeyboard := tgbotapi.InlineKeyboardMarkup{}
+					for eachAccount := range db[chatId] {
+						var row []tgbotapi.InlineKeyboardButton
+						button := tgbotapi.NewInlineKeyboardButtonData(eachAccount, eachAccount)
+						row = append(row, button)
+						deleteAccountsKeyboard.InlineKeyboard = append(deleteAccountsKeyboard.InlineKeyboard, row)
+					}
 
-			if update.Message.From.IsBot {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, assets.Texts["do_not_work_with_bots"])
+					msg.Text = assets.Texts["account_choose_to_delete"]
+					msg.ReplyMarkup = deleteAccountsKeyboard
+				default:
+					log.Printf("COMMAND UNKNOWN %s (ID %d)", update.Message.From.UserName, update.Message.From.ID)
+					msg.Text = assets.Texts["unknown_command"]
+				}
 				bot.Send(msg)
-				continue
-			}
-			
-			privateStatus, err := api.GetPrivateStatus(insta, strings.ToLower(update.Message.Text))
-			if err != nil { // любая ошибка при проверке статуса
-				log.Printf("ADD error %s, %v", strings.ToLower(update.Message.Text), err)
-				insta, err = api.GetApi(config.IgUsername1, config.IgPassword1)
-				if err != nil { // ошибка авторизации
-					log.Printf("ADD ERROR login %s, %v", config.IgUsername1, err)
-					insta, err = api.GetApi(config.IgUsername2, config.IgPassword2)
-					if err != nil { // ошибка авторизации
-						log.Printf("ADD ERROR login %s, %v", config.IgUsername2, err)
+			} else {
+				// тут добавляем новый аккаунт
+
+				newAccountName := strings.ToLower(update.Message.Text)
+
+				log.Printf("ADD try %s BY %s (ID %d)", newAccountName, update.Message.From.UserName, update.Message.From.ID)
+				if _, ok := db[chatId]; !ok {
+					db[chatId] = make(account)
+				}
+
+				if len(db[chatId]) > 2 {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, assets.Texts["limit_of_accounts"])
+					bot.Send(msg)
+					continue
+				}
+
+				if strings.Contains(newAccountName, " ") {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], assets.Texts["account_not_found"]))
+					bot.Send(msg)
+					continue
+				}
+
+				if update.Message.From.IsBot {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, assets.Texts["do_not_work_with_bots"])
+					bot.Send(msg)
+					continue
+				}
+
+				//TODO надо выработать единые правила для нулевой инсты - брать паузу для логина (как-то через крон) или выключать бота
+				//TODO скорее всего этот блок нужно удалить, потому, что insta с нулем сюда не сможет попасть (или сможет?)
+				//TODO скорее всего не сможет, потому что на старте происход проверка, если после загрузки ноль, то паника и бот выключается
+				// if insta == nil {
+				// 	log.Printf("ADD error %s", newAccountName)
+				// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], assets.Texts["endpoint_error"]))
+				// 	bot.Send(msg)
+				////isPanic = true
+				// 	continue
+				// }
+
+				privateStatus, err := api.GetPrivateStatus(insta, newAccountName)
+				if err == api.UserNotFoundError { // ошибка "account_not_found"
+					log.Printf("ADD error %s, %v", newAccountName, err)
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], assets.Texts["account_not_found"]))
+					bot.Send(msg)
+					continue
+				} else if err != nil { // ошибка при проверке статуса кроме "account_not_found"
+					log.Printf("ADD ERROR %s, %v", newAccountName, err)
+					insta = api.GetNewApi(igAccounts)
+					if insta == nil { // ошибка авторизации
+						//TODO если инста нулевая, то потом может произойти что угодно,
+						//TODO тут надо прекращать таск и брать паузу для логина (как-то через крон) или выключать бота
+						////isPanic = true
+						log.Print("ADD ERROR login")
 						msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], err.Error()))
 						bot.Send(msg)
 						continue
-					} else {
-						privateStatus, err = api.GetPrivateStatus(insta, strings.ToLower(update.Message.Text))
-						if err != nil { // это ошибка не связанная с логином, возможно поменялось имя акканта
-							log.Printf("ADD error %s, %v", strings.ToLower(update.Message.Text), err)
+					} else { // авторизация прошла успешно
+						privateStatus, err = api.GetPrivateStatus(insta, newAccountName)
+						if err == api.UserNotFoundError { // ошибка "account_not_found"
+							log.Printf("ADD error %s, %v", newAccountName, err)
 							msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], assets.Texts["account_not_found"]))
+							bot.Send(msg)
+						} else if err != nil {
+							log.Print("ADD ERROR login")
+							msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], err.Error()))
 							bot.Send(msg)
 							continue
 						}
 					}
-				} else {
-					privateStatus, err = api.GetPrivateStatus(insta, strings.ToLower(update.Message.Text))
-					if err != nil { // это ошибка не связанная с логином, возможно поменялось имя акканта
-						log.Printf("ADD error %s, %v", strings.ToLower(update.Message.Text), err)
-						msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_add_error"], assets.Texts["account_not_found"]))
-						bot.Send(msg)
-						continue
-					}
 				}
+
+				db[chatId][newAccountName] = privateStatus
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_added"], newAccountName))
+				log.Printf("ADD success %s BY %s (ID %d)", newAccountName, update.Message.From.UserName, update.Message.From.ID)
+				msg.ParseMode = "HTML"
+				saveData(db, config)
+				bot.Send(msg)
 			}
 
-			newAccountName := strings.ToLower(update.Message.Text)
-			db[chatId][newAccountName] = privateStatus
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf(assets.Texts["account_added"], newAccountName))
-			log.Printf("ADD success %s BY %s (ID %d)", newAccountName, update.Message.From.UserName, update.Message.From.ID)
-			msg.ParseMode = "HTML"
-			saveData(db, config)
-			bot.Send(msg)
 		}
 
 	}
